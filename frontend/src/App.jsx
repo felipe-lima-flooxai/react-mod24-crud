91338f181f3b215fc9518192e1b30edf5a9e43cf
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-12 11:27:27 -0300

    Nav mt ruim

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import './App.css'
--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -3,6 +3,14 @@ import React from "react"
 
 export default props => (
     <aside className="menu-area">
-        Naveg
+        <nav className="menu">
+            {/* Precisa refatorar isso aqui, ta mt cru */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
 )
